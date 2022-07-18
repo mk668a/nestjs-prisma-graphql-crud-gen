ac5ea9ceada0135f821e8369dee9e040eca7adba
mk668a <msftoaa@gmail.com>
2022-07-19 01:49:34 +0900

    generate models

---
+++ packages/generator/src/generator/dmmf/DmmfDocument.ts
@@ -0,0 +1,73 @@
+import { DMMF as PrismaDMMF } from '@prisma/client/runtime'
+import { DMMF } from './types'
+import { transformSchema, transformMappings, transformBareModel, transformModelWithFields, transformEnums, generateRelationModel } from './transform'
+import { EmitBlockKind, GeneratorOptions } from '../options'
+
+export class DmmfDocument implements DMMF.Document {
+  private models: DMMF.Model[]
+  datamodel: DMMF.Datamodel
+  schema: DMMF.Schema
+  enums: DMMF.Enum[]
+  modelMappings: DMMF.ModelMapping[]
+  relationModels: DMMF.RelationModel[]
+
+  constructor({ datamodel, schema, mappings }: PrismaDMMF.Document, public options: GeneratorOptions) {
+    const enumTypes = [...(schema.enumTypes.prisma ?? []), ...(schema.enumTypes.model ?? [])]
+    const models = [...datamodel.models, ...datamodel.types]
+
+    // transform bare model without fields
+    this.models = models.map(transformBareModel)
+    // transform enums before model fields to map enum types to enum values string union
+    this.enums = enumTypes.map(transformEnums(this))
+    // then transform once again to map the fields (it requires mapped model type names)
+    this.models = models.map(transformModelWithFields(this))
+    // transform enums again to map renamed fields
+    this.enums = enumTypes.map(transformEnums(this))
+
+    this.datamodel = {
+      models: this.models,
+      enums: datamodel.enums.map(transformEnums(this)),
+      types: [],
+    }
+    this.schema = {
+      ...transformSchema(schema, this),
+      enums: this.enums,
+    }
+    this.modelMappings = transformMappings(mappings.modelOperations, this, options)
+    this.relationModels = this.models
+      .filter((model) => model.fields.some((field) => field.relationName !== undefined && !field.isOmitted.output))
+      .filter((model) => {
+        const outputType = this.schema.outputTypes.find((type) => type.name === model.name)
+        return (
+          outputType &&
+          outputType.fields.some((outputTypeField) =>
+            model.fields.some(
+              (modelField) => modelField.name === outputTypeField.name && modelField.relationName !== undefined && !modelField.isOmitted.output,
+            ),
+          )
+        )
+      })
+      .map(generateRelationModel(this))
+  }
+
+  getModelTypeName(modelName: string): string | undefined {
+    return this.models.find((it) => it.name.toLocaleLowerCase() === modelName.toLocaleLowerCase())?.typeName
+  }
+
+  isModelName(typeName: string): boolean {
+    return this.models.some((it) => it.name === typeName)
+  }
+
+  isModelTypeName(typeName: string): boolean {
+    return this.models.some((it) => it.typeName === typeName)
+  }
+
+  getModelFieldAlias(modelName: string, fieldName: string): string | undefined {
+    const model = this.models.find((it) => it.name === modelName)
+    return model?.fields.find((it) => it.name === fieldName)?.typeFieldAlias
+  }
+
+  shouldGenerateBlock(block: EmitBlockKind): boolean {
+    return this.options.blocksToEmit.includes(block)
+  }
+}
---
+++ packages/generator/src/generator/dmmf/helpers.ts
@@ -0,0 +1,34 @@
+export const modelAttributeRegex = /(@@NestJsGraphQL\.)+([A-z])+(\()+(.+)+(\))+/
+export const fieldAttributeRegex = /(@NestJsGraphQL\.)+([A-z])+(\()+(.+)+(\))+/
+export const attributeNameRegex = /(?:\.)+([A-Za-z])+(?:\()+/
+export const attributeArgsRegex = /(?:\()+([A-Za-z])+\:+(.+)+(?:\))+/
+
+export function parseDocumentationAttributes<TData extends object = object>(
+  documentation: string | undefined,
+  expectedAttributeName: string,
+  expectedAttributeKind: 'model' | 'field',
+): Partial<TData> {
+  const attributeRegex = expectedAttributeKind === 'model' ? modelAttributeRegex : fieldAttributeRegex
+  const attribute = documentation?.match(attributeRegex)?.[0]
+  const attributeName = attribute?.match(attributeNameRegex)?.[0]?.slice(1, -1)
+  if (attributeName !== expectedAttributeName) {
+    return {}
+  }
+  const rawAttributeArgs = attribute?.match(attributeArgsRegex)?.[0]?.slice(1, -1)
+  const parsedAttributeArgs: Record<string, unknown> = {}
+  if (rawAttributeArgs) {
+    const rawAttributeArgsParts = rawAttributeArgs
+      .split(':')
+      .map((it) => it.trim())
+      .map((part) => (part.startsWith('[') ? part : part.split(',')))
+      .flat()
+      .map((it) => it.trim())
+
+    for (let i = 0; i < rawAttributeArgsParts.length; i += 2) {
+      const key = rawAttributeArgsParts[i]
+      const value = rawAttributeArgsParts[i + 1]
+      parsedAttributeArgs[key] = JSON.parse(value)
+    }
+  }
+  return parsedAttributeArgs as Partial<TData>
+}
